f91b6a1003ffdaa80c8b2f14e7d5eeb364d4b2a6
kondoing <kondo.shunsuke@ist.osaka-u.ac.jp>
2017-07-11 09:30:41 +0900

    urg_sensor.c fixed
    USTのstable状態("sensor is working normally")に対応するために修正

--- current/src/urg_sensor.c
+++ current/src/urg_sensor.c
@@ -1220,7 +1220,7 @@ void urg_wakeup(urg_t *urg)
 int urg_is_stable(urg_t *urg)
 {
     const char *stat = urg_sensor_status(urg);
-    return (strstr(stat,"Stable") != NULL) ||(strstr(stat, "well") != NULL) ? 1: 0;  
+    return (strstr(stat,"Stable") != NULL) || (strstr(stat, "well") != NULL) || (strstr(stat, "normally") != NULL) ? 1: 0;  
 }
 
 
